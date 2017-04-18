bbd05bae8c955676ba42307394278e596becf6d9
ramendushandilya <ramendu.shandilya@gmail.com>
2017-04-18 14:26:27 +0530

    random test

--- src/algorithms/searchAndSort/binarySearch/Runner.java
+++ src/algorithms/searchAndSort/binarySearch/Runner.java
@@ -24,6 +24,7 @@ public class Runner {
     public static void main(String[] args) {
         int[] a= {0, 1, 1, 2, 3, 5, 8, 13, 21,
                 34, 55, 89, 144, 233, 377, 610};
+        int[] b = {};
         System.out.print(jumpSearch(a, 610));
     }
 
